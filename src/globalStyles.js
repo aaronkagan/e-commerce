50b1659ce46c762bcd65685cda1f080456026afd
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-02 16:47:50 -0500

    adding to global style

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,7 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    padding:0
+    padding:0;
+    font-family: sans-serif;
   }
 
   ul {
