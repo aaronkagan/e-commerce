4f6d76b011258995ac07e3fa4bd2d5b27977362a
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-03 15:25:47 -0500

    styling homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
+import { width } from '@mui/system';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import banner from '../assets/images/banner.jpg';
+import banner from '../assets/images/banner.jpeg';
+import ProductItem from './ProductItem';
 
 const Homepage = () => {
-  const [products, setProducts] = useState([{}]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products?limit=7')
@@ -18,15 +20,92 @@ const Homepage = () => {
         src={banner}
         alt="banner"
       />
+      <ProductsContainer>
+        {products.length > 0 ? (
+          <>
+            <Row1>
+              <ProductItem
+                row={1}
+                item={products[0]}
+              />
+              <ProductItem
+                row={1}
+                item={products[1]}
+              />
+            </Row1>
+            <Row2>
+              <ProductItem
+                row={2}
+                item={products[2]}
+              />
+              <ProductItem
+                row={2}
+                item={products[3]}
+              />
+              <ProductItem
+                row={2}
+                item={products[4]}
+              />
+            </Row2>
+            <Row3>
+              <ProductItem
+                row={3}
+                item={products[5]}
+              />
+              <ProductItem
+                row={3}
+                item={products[6]}
+              />
+            </Row3>
+          </>
+        ) : (
+          <Loading>Loading</Loading>
+        )}
+      </ProductsContainer>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  background: #0082ec7d;
+`;
 
 const Banner = styled.img`
-  width: 100vw;
-  height: 300px;
+  width: 100%;
+  height: 400px;
+  z-index: -1;
+  margin-bottom: -50px;
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+`;
+
+const ProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-bottom: 3vh;
+`;
+
+const Row1 = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 95%;
+`;
+
+const Row2 = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 95%;
+`;
+
+const Row3 = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 95%;
+`;
+
+const Loading = styled.h3`
+  height: 100vh;
 `;
 
 export default Homepage;
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,16 +1,53 @@
 import styled from 'styled-components';
 
-const ProductItem = ({ item }) => {
+const ProductItem = ({ item, row }) => {
   return (
-    <Wrapper>
-      <img
-        src={item.image}
-        alt={item.title}
-      />
+    <Wrapper row={row}>
+      <p>{item.title}</p>
+      <p>${item.price}</p>
+      <Rating>⭐️⭐️</Rating>
+      <Img src={item.image} />
+      <Button>Add Item</Button>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+  width: ${(props) => {
+    return props.row % 2 === 0 ? '33.3%' : '50%';
+  }};
+  max-height: 400px;
+  min-width: 100px;
+  background-color: white;
+`;
+
+const Rating = styled.div`
+  display: flex;
+`;
+
+const Img = styled.img`
+  max-height: 200px;
+  width: 100%;
+  object-fit: contain;
+  margin: 20px 0 0 15px;
+`;
+
+const Button = styled.button`
+  margin: 20px auto;
+  border: none;
+  background-color: #ff9f00;
+  padding: 5px 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
 
 export default ProductItem;
