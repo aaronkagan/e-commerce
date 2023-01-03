9a1ba0a49544990924d9fda8aa89e7c764aa3bbc
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-02 20:39:26 -0500

    fetching product data

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import banner from '../assets/images/banner.jpg';
 
 const Homepage = () => {
-  return <Wrapper>Homepage</Wrapper>;
+  const [products, setProducts] = useState([{}]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Wrapper>
+      <Banner
+        src={banner}
+        alt="banner"
+      />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div``;
+
+const Banner = styled.img`
+  width: 100vw;
+  height: 300px;
+`;
 
 export default Homepage;
