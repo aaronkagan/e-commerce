2e45442e0c29f12e7c8c2504208dd76b196bc3c2
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-06 14:22:15 -0500

    adding to cart setup

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,38 @@
 import styled from 'styled-components';
+import banner from '../assets/images/cart-banner.png';
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <Wrapper>
+      <Basket>
+        <Banner
+          src={banner}
+          alt="Boxing Day Sale"
+        />
+        <Title>Your Cart</Title>
+      </Basket>
+      <ProductsArea></ProductsArea>
+      <PayArea></PayArea>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 2%;
+`;
+
+const Basket = styled.div`
+  width: 70%;
+`;
+
+const Banner = styled.img`
+  width: 100%;
+`;
+
+const Title = styled.h2``;
+
+const ProductsArea = styled.div``;
+
+const PayArea = styled.div``;
 
 export default Cart;
