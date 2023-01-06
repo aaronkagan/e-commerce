11ce7e465d54ff54699a07be0beb870338ce367e
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-06 13:47:09 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from './components/Header';
 import Homepage from './components/Homepage';
 import GlobalStyle from './globalStyles';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart';
 const App = () => {
   return (
     <div>
       <GlobalStyle />
       <Header />
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route
+            path="/cart"
+            element={<Cart />}
+          />
+          <Route
+            path="/"
+            element={<Homepage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 };
